2f62df499532cdf6c8da8b3788f7780aac945ddd
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-20 16:09:38 -0600

    write logout function

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,14 @@
-// what happens when you end session. 
+const logout = async () => {
+    const resp = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (resp.ok) {
+        document.location.replace('/');
+    } else {
+        alert(resp.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
