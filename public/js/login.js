0f90d62de828b9108757e145d371964dcfe9bdfa
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-20 15:17:10 -0600

    move login js file from homepage to correct login file

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,45 @@
-// what happens when you create session
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email.login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+
+        const resp = await fetch ('/api/users/login', {
+            method: 'POST', 
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (resp.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(resp.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const resp = await fetch('/api/users', {
+            method: 'POST', 
+            body: JSON.stringify({ username, password }), 
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (resp.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(resp.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
