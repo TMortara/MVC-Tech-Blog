70f121b6d6160b95d09bfff61282fcdec30ba6bc
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-26 14:36:19 -0600

    create seedall function in seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,16 @@
 const sequelize = require('../config/connection');
+const seedUsers = require('./userData.json');
+const seedPosts = require('./blogData.json');
+const seedComments = require('./commentData.json');
+
+const seedAll = async () => {
+    await sequelize.sync({ force:true });
+
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+
+    process.exit(0);
+};
+
+seedAll();
